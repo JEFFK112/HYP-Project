32ebce191c7f13a19f222acfaa3a4fb4e3f0ea2e
Jeff <mahdijafer995@gmail.com>
2024-05-07 23:55:39 +0300

    .

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+
+export const PORT = 5555;
+export const mongoDBURL = "mongodb+srv://mahdijafer995:EJwgFFnXnR3RFV8K@books-store-mern.mxjdozi.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN";
