6c691792730313539faf0bbdf538b7d2b586eda5
Jeff <mahdijafer995@gmail.com>
2024-05-25 16:55:40 +0300

    Controller-Update

--- backend/index.js
+++ backend/index.js
@@ -23,11 +23,6 @@ app.use(cors());
 //     })
 // );
 
-app.get('/', (request,response)=>{
-    console.log(request);
-    return response.status(234).send('Welcome');
-});
-
 app.use('/books', booksRoute);
 
 mongoose
